015f1d2a015896ccb26e0edee008c9363e505339
Lakshya Singh Panwar <lakshya.s.panwar@gmail.com>
2024-06-17 23:48:45 -0400

    make thread-safe + add inc method

--- main.go
+++ main.go
@@ -2,31 +2,45 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"hash/fnv"
 	"log"
 	"net"
 	"net/http"
+	"sync"
 )
 
 type KVStore struct {
 	size int
-	data map[uint64]string // [key]value
+	data map[uint64]int // [key]value
+	mu sync.Mutex
 }
-func (m *KVStore) set(k string, v string) {
+func (m *KVStore) set(k string, v int) {
 	h := hash(k)
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	if _, ok := m.data[h]; !ok {
 		m.size++
 	}
 	m.data[hash(k)] = v;
 }
-func (m *KVStore) get(k string) (string, error) {
+func (m *KVStore) get(k string) (int, error) {
 	h := hash(k)
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	val, ok := m.data[h]
 	if ok {
 		return val, nil
 	}
 	return val, errors.New("this key does not exist in the map") 
+}
+
+func (m *KVStore) inc(k string) {
+	val, err := m.get(k)
+	if err != nil {
+		m.set(k, 1)
+		return
+	}
+	m.set(k, val + 1)
 }
 
 func hash(id string) uint64 {
@@ -36,20 +50,15 @@ func hash(id string) uint64 {
 }
 
 func main() {
-	m := KVStore{size: 0, data: make(map[uint64]string)}
+	m := KVStore{size: 0, data: make(map[uint64]int)}
 	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("welcome")
-		fmt.Println(r.RemoteAddr)
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			log.Fatal("Invalid IP address")
 		}
-		fmt.Println(ip)
 		
-		m.set(ip, "RAHH")
-		val, _ := m.get(ip)
-		fmt.Println(val)
+		m.inc(ip)
 	});
 
 	http.ListenAndServe(":8080", nil)
